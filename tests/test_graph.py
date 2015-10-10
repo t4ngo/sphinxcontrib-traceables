b7939c13622d1134364e874da1d1903bcea6cffe
t4ngo <t4ngo@twizzy.biz>
2015-10-10 16:27:55 +0200

    Remove debug printing of doctree during a test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -11,7 +11,6 @@ from utils import with_app, pretty_print_xml
 def test_graph(app, status, warning):
     app.build()
     tree = ElementTree.parse(app.outdir / "index.xml")
-    pretty_print_xml(tree.getroot())
 
     # Verify that 1 graphviz node is found.
     assert len(tree.findall(".//graphviz")) == 1
