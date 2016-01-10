2486d01dd603387637f390b963e53afedbab82ec
t4ngo <t4ngo@twizzy.biz>
2016-01-10 16:15:56 +0100

    Expand tests for traceable matrix output

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -8,9 +8,32 @@ from utils import with_app, pretty_print_xml
 # Tests
 
 @with_app(buildername="xml", srcdir="matrix", warningiserror=True)
-def test_list(app, status, warning):
+def test_matrix_structure(app, status, warning):
+    '''Verify generated XML structure for different matrix formats'''
     app.build()
 
-    # Verify that ...
-    tree = ElementTree.parse(app.outdir / "matrix_basic.xml")
+    # Verify matrix with default format.
+    tree = ElementTree.parse(app.outdir / "matrix_default.xml")
+    # Verify that the correct number of list items were generated.
     assert len(tree.findall(".//list_item")) == 11
+
+    # Verify matrix with columns format.
+    tree = ElementTree.parse(app.outdir / "matrix_columns.xml")
+#    pretty_print_xml(tree.getroot())
+    # Verify that the correct number of rows and entries were generated.
+    rows = tree.findall(".//tbody/row")
+    assert len(rows) == 3
+    assert len(rows[0].findall("./entry")) == 2
+
+    # Verify matrix with table format.
+    tree = ElementTree.parse(app.outdir / "matrix_table.xml")
+#    pretty_print_xml(tree.getroot())
+    # Verify that the correct number of rows and entries were generated.
+    rows = tree.findall(".//tbody/row")
+    assert len(rows) == 3
+    assert len(rows[0].findall("./entry")) == 3
+
+@with_app(buildername="latex", srcdir="matrix", warningiserror=True)
+def test_latex_builder(app, status, warning):
+    '''Verify that Latex builder doesn't fail'''
+    app.build()
