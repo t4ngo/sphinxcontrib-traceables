cbb5f48b37d23dd5e706f816cd194e8797b4cbcc
t4ngo <t4ngo@twizzy.biz>
2016-08-13 19:40:02 +0200

    Add usage example of requirements management

---
+++ examples/requirements/conf.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+# -------------------------------------------------------------------------
+# Configure extensions
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.traceables',
+]
+
+sys.path.append(os.path.abspath('../../tests'))
+
+# -------------------------------------------------------------------------
+# General configuration
+
+project = u'Traceables example project: Requirements'
+copyright = u'2016, t4ngo'
+version = '0.2'                        # The short X.Y version.
+release = '0.2'                        # The full version, incl alpha/beta/rc.
+
+templates_path = ['_templates']
+source_suffix = '.txt'                 # The suffix of source filenames.
+master_doc = 'index'                   # The master toctree document.
+today_fmt = '%Y-%m-%d'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+keep_warnings = True                   # Keep warnings in output documents.
+
+rst_prolog = '''
+.. include:: <isoamsa.txt>
+'''
+
+# -------------------------------------------------------------------------
+# Configure HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
+html_static_path = ['_static']
+html_show_sourcelink = True            # Link to source from pages.
+
+#latex_elements = {
+#    "preamble": u"""
+#\\usepackage{newunicodechar}
+#\\newunicodechar{\u2714}{\\checkmark}
+#""",
+#}
+
+traceables_relationships = [
+    ("children", "parents", True),
+]
